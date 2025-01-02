fc95c9062166b53095a34235d963d88603ee59e2
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-01-03 01:54:55 +0800

    sitemap & registration

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://notedcebu.vercel.app',
+            url: 'https://noted-cebu.vercel.app/',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1
